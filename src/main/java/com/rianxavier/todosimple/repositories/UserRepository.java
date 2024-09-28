ba7f86085826f3fbbf112b97de4d65a428ded1be
Rian Xavier <rianalvesxvr@gmail.com>
2024-09-28 15:43:40 -0300

    Create User and Task repositories

---
+++ src/main/java/com/rianxavier/todosimple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rianxavier.todosimple.repositories;
+
+import com.rianxavier.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
