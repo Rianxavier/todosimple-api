77f175c8c4f218099271120061dad90056550196
Rian Xavier <rianalvesxvr@gmail.com>
2024-10-03 11:34:25 -0300

    Create WebConfig

--- src/main/java/com/rianxavier/todosimple/config/WebConfig.java
+++ src/main/java/com/rianxavier/todosimple/config/WebConfig.java
@@ -1,2 +1,15 @@
-package com.rianxavier.todosimple.config;public class WebConfig {
+package com.rianxavier.todosimple.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
