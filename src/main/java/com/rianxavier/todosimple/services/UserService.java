4352c81ae4ea29911c9c24c2f4d22a4657b24bb2
Rian Xavier <rianalvesxvr@gmail.com>
2024-10-15 11:38:50 -0300

    Set UserService authorization

--- src/main/java/com/rianxavier/todosimple/services/UserService.java
+++ src/main/java/com/rianxavier/todosimple/services/UserService.java
@@ -3,13 +3,17 @@ package com.rianxavier.todosimple.services;
 import com.rianxavier.todosimple.models.User;
 import com.rianxavier.todosimple.models.enums.ProfileEnum;
 import com.rianxavier.todosimple.repositories.UserRepository;
+import com.rianxavier.todosimple.security.UserSpringSecuriry;
+import com.rianxavier.todosimple.services.exceptions.AuthorizationException;
 import com.rianxavier.todosimple.services.exceptions.DataBindingViolationException;
 import com.rianxavier.todosimple.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,6 +29,11 @@ public class UserService {
 
 
     public User findById(Long id) {
+        UserSpringSecuriry userSpringSecuriry = authenticated();
+        if (!Objects.nonNull(userSpringSecuriry)
+                || !userSpringSecuriry.hasRole(ProfileEnum.ADMIN) && !id.equals(userSpringSecuriry.getId()))
+            throw new AuthorizationException("Acesso negado!");
+
         Optional<User> user = this.userRepository.findById(id);
 
         return user.orElseThrow(() -> new ObjectNotFoundException(
@@ -42,18 +51,26 @@ public class UserService {
     }
 
     @Transactional
-    public User update(User obj){
+    public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         findById(id);
         try {
             this.userRepository.deleteById(id);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+
+    public static UserSpringSecuriry authenticated() {
+        try {
+            return (UserSpringSecuriry) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            return null;
         }
     }
 
